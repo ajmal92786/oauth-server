0412261fd58addda9f4391bae2f8f78d6df6b1e2
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2025-06-14 18:08:14 +0530

    refactor: move Google redirect_uri to environment variable

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.get("/auth/github/callback", async (req, res) => {
 });
 
 app.get("/auth/google", (req, res) => {
-  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}&response_type=code&scope=profile email`;
 
   res.redirect(googleAuthUrl);
 });
@@ -108,7 +108,7 @@ app.get("/auth/google/callback", async (req, res) => {
         client_secret: process.env.GOOGLE_CLIENT_SECRET,
         code,
         grant_type: "authorization_code",
-        redirect_uri: `http://localhost:${PORT}/auth/google/callback`,
+        redirect_uri: process.env.GOOGLE_REDIRECT_URI,
       },
       {
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
