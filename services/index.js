618d533cd338f8461c7837530ff60d38f23ed137
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2025-06-14 17:45:32 +0530

    fix: set secure cross-origin cookie for frontend integration

--- services/index.js
+++ services/index.js
@@ -4,6 +4,8 @@ function setSecureCookie(res, token) {
   res.cookie("access_token", token, {
     httpOnly: true,
     maxAge: 60 * 1000, // 1 minute
+    secure: true,
+    sameSite: "None",
   });
 
   return res;
